aa473261c6a59c3342e5fa8c93ac902a30ba188a
ellijahcuts <ellijahcuts@gmail.com>
2022-03-05 00:12:12 +0300

    1. Start Learn PHP

--- demo/test.php
+++ demo/test.php
@@ -1,1 +1,43 @@
-<?
+<p>One</p>
+<?php
+echo "Hello, world!";
+?>
+
+<p>Two</p>
+<?php
+print "Hello, planet !";
+?>
+
+<p>Three</p>
+<?php
+echo "Hello", ", ", "planet !";
+?>
+
+<p>Four</p>
+<?php
+echo strftime("%d-%M-%Y")
+?>
+
+<p>Five</p>
+<?php
+$x = 10;
+$x = $x + 12;
+echo $x;
+?>
+<?php
+$name = "Psina";
+?>
+<p>Six</p>
+<h3>Привет, слыш ты <?= $name ;?></h3>
+
+<p>Seven</p>
+<?php
+define("USER_NAME", "ILYA");
+const NAME_USER = "PISOS";
+echo USER_NAME," ", NAME_USER;
+?>
+
+
+<?php
+//Остановился в пером дне 1.55.40
+?>
